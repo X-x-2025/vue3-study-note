93f620dcf95b6ea905a3dd29f3e5f6be268b4a04
X-x-2025 <2084109809@qq.com>
2025-07-25 17:55:54 +0800

    模板引用

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,19 @@
 <script setup>
-// 父传子
-// 1.给子组件添加属性的方式传值
-// 2.在子组件通过props接收
-
-// 子传父
-// 1.在子组件内部，emit触发事件
-// 2.在父组件通过@监听
+// 模板引用
+// 概念：通过ref标识获取真实的dom对象或者组件实例对象
+// 语法
 import { ref } from 'vue';
-// 局部导入子组件
-import SonComonent from './components/SonComonent.vue';
-// 定义响应式数据
-const money = ref(1000000)
-const makeMoney = () => {
-  money.value += 10000
-}
-const changeMoneyfn = (costvalue) => {
-  money.value -= costvalue
-}
+// 1.调用ref函数生成一个ref对象
+const h2Ref = ref(null)
+
 
 </script>
 
 <template>
+  <!-- 2.通过ref标识绑定ref对象 -->
+  <h2 ref="h2Ref"></h2>
   <div>
-    <h2>父组件资产{{ money }}
-      <!-- 动态控制 -->
-      <button style="border: 1px solid white;" @click="makeMoney">挣钱</button>
-    </h2>
-    <!-- 给子组件添加属性的方式传值 -->
-    <SonComonent 
-    car="宝马" 
-    :money="money"
-    @changeMoney="changeMoneyfn"
-    ></SonComonent>
+    <input type="text">
+    <button>点击输入框聚焦</button>
   </div>
 </template>
